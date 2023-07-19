3793bbd0cac37d51e1e215ed0d0868282afbe8c8
polina-koleva-fourth <polina.koleva@fourth.com>
2023-07-19 12:16:23 +0300

    add models

---
+++ TravelEasy.ElectricVehicles.DB.Models/ElectricVehicle.cs
@@ -0,0 +1,17 @@
+﻿namespace TravelEasy.ElectricVehicles.DB.Models
+{
+    public class ElectricVehicle
+    {
+        public int Id { get; set; }
+
+        public string? Brand { get; set; }
+
+        public string? Model { get; set; }
+
+        public int HorsePowers { get; set; }
+
+        public int Range { get; set; }
+
+        public decimal PricePerDay { get; set; }
+    }
+}
