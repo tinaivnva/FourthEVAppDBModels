89867ab994afa0df4d24fddd7904e8f61d2b083b
tsvetymanolova <ts.manolova23@gmail.com>
2023-07-25 13:19:40 +0300

    updated Booking

--- TravelEasy.ElectricVehicles.DB.Models/Booking.cs
+++ TravelEasy.ElectricVehicles.DB.Models/Booking.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -9,8 +10,12 @@ namespace TravelEasy.ElectricVehicles.DB.Models
 {
     public class Booking
     {
+        [Key]
+        public int Id { get; set; }
         public int UsertId { get; set; }
-        public int CarId { get; set; }
-        public 
+        public int CarId { get; set; } 
+        public DateTime FromDate {get; set; }
+        public DateTime ToDate { get; set; }
+
     }
 }
